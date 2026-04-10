d2789f05a1139deb40aa2a538aed2375e3528f7e
Joel Forero <joelforero5@gmail.com>
2026-04-10 13:16:10 -0400

    validacion de formularios

--- src/components/main/components/popup/editProfile/EditProfile.jsx
+++ src/components/main/components/popup/editProfile/EditProfile.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react'; 
+import { useState, useContext, useRef } from 'react'; 
 import {CurrentUserContext} from '../../../../../contexts/CurrentUserContext.js'; 
 
 export default function EditProfile() {
@@ -7,24 +7,33 @@ export default function EditProfile() {
 
   const [name, setName] = useState(currentUser.name); // Agrega la variable de estado para name
   const [description, setDescription] = useState(currentUser.about); // Agrega la variable de estado para description
+  const [isValid,setIsValid] = useState(false);
+  const [errors,setErrors] = useState({name:'',description:''});
+  const nameRef = useRef();
+  const descriptionRef = useRef();
 
   const handleNameChange = (event) => {
-    setName(event.target.value); // Actualiza name cuando cambie la entrada
+    setName(event.target.value);
+        setErrors((prev) => ({ ...prev, name: event.target.validationMessage }));
+       setIsValid(event.target.form.checkValidity());
   };
 
   const handleDescriptionChange = (event) => {
-    setDescription(event.target.value); // Actualiza description cuando cambie la entrada
+     setDescription(event.target.value);
+        setErrors((prev) => ({ ...prev, description: event.target.validationMessage }));
+        setIsValid(event.target.form.checkValidity());
   };
-
   const handleSubmit = (event) => {
     event.preventDefault(); // Evita el comportamiento predeterminado del envío de formularios
 
     handleUpdateUser({ name, about: description }); // Actualiza la información del usuario
   };
+ 
 
     return(
     <form autoComplete="off" className="popup__form" id="edit-profile-form" onSubmit={handleSubmit}>
     <input
+    ref={nameRef}
         className="popup__input popup__input_type_name"
         id="name"
         name="name"
@@ -36,8 +45,9 @@ export default function EditProfile() {
         onChange={handleNameChange}
         value={name} // Agrega el valor del estado name al input
     />
-    <span className="popup__input-error" id="name-error"></span>
+    <span className={`popup__input-error ${errors.name && 'popup__input-error_visible'}`} id="name-error">{errors.name}</span>
     <input
+    ref={descriptionRef}
         className="popup__input popup__input_type_description"
         id="description"
         name="description"
@@ -49,8 +59,8 @@ export default function EditProfile() {
         onChange={handleDescriptionChange}
         value={description} // Agrega el valor del estado description al input
     />
-    <span className="popup__input-error" id="description-error"></span>
-    <button className="button popup__button" type="submit" >Guardar</button>
+    <span className={`popup__input-error ${errors.description && 'popup__input-error_visible'}`} id="description-error">{errors.description}</span>
+    <button className="button popup__button" type="submit" disabled={!isValid}>Guardar</button>
 </form>
 );
 }
--- src/components/main/components/popup/newCard/NewCard.jsx
+++ src/components/main/components/popup/newCard/NewCard.jsx
@@ -1,12 +1,19 @@
-import { useContext, useRef } from "react";
+import { useContext, useRef , useState  } from "react";
 import { CurrentUserContext } from "../../../../../contexts/CurrentUserContext.js";
 export default function NewCard(){
 const {handleAddPlaceSubmit} = useContext(CurrentUserContext);
 const namePLaceRef = useRef();
 const linkPlaceRef = useRef();
+const [isValid,setIsValid] = useState(false);
+const [errors,setErrors] = useState({name:'',link:''});
 function handleSubmit(event){
     event.preventDefault();
     handleAddPlaceSubmit({name:namePLaceRef.current.value, link:linkPlaceRef.current.value});
+}
+function handleInputChange(event){
+    const {name, validationMessage} = event.target;
+    setErrors((prev) => ({ ...prev, [name]: validationMessage }));
+    setIsValid(event.target.form.checkValidity());
 }
 return(
     <form autoComplete="off" className="popup__form" id="new-card-form" onSubmit={handleSubmit}>
@@ -20,8 +27,9 @@ return(
               type="text"
               minLength="2"
               maxLength="30"
+              onChange={handleInputChange}
             />
-            <span className="popup__input-error popup__input-error_visible" id="place-name-error"></span>
+            <span className={`popup__input-error ${errors.name && 'popup__input-error_visible'}`} id="place-name-error">{errors.name}</span>
             <input
               ref={linkPlaceRef}
               className="popup__input popup__input_type_url"
@@ -31,9 +39,11 @@ return(
               required
               type="url"
               minLength="2"
+              onChange={handleInputChange}
+
             />
-            <span className="popup__input-error" id="link-error"></span>
-            <button className="button popup__button" type="submit">Crear</button>
+            <span className={`popup__input-error ${errors.link && 'popup__input-error_visible'}`} id="link-error">{errors.link}</span>
+            <button className="button popup__button" type="submit" disabled={!isValid}>Crear</button>
           </form>
 );
 }
